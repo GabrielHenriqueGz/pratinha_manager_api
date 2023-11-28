1e63ee56a0c49394845933c74eeb46b0478cfc64
Gabriel Henrique <gazao741@gmail.com>
2023-11-28 19:07:29 -0300

    Adicionando ping no server para teste e evitar inatividade

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,17 @@
 import { Bot } from './bot';
+import Fastify from 'fastify';
+
+const app = Fastify();
+
+app.get('/', function (request, reply) {
+    reply.send({ data: new Date() })
+});
 const discordBot = new Bot();
 discordBot.startBot();
+app.listen({ port: 8001 }, (err, address) => {
+    if (err) {
+        app.log.error(err)
+        process.exit(1)
+    }
+    app.log.info(`Server is now listening on ${address}`)
+})
